31f015c2e7268c46ebf7b2cd5608d48bc077c953
IRCNET <ircd@evilnet.org>
2025-12-18 11:51:53 -0500

    Adding WEBIRC support

---
+++ common/patchlevel.h
@@ -0,0 +1,34 @@
+/************************************************************************
+ *   IRC - Internet Relay Chat, common/patchlevel.h
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 1, or (at your option)
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifndef	PATCHLEVEL
+#define PATCHLEVEL "0211020003"		/* for server identification */
+#define DEVLEVEL 'a'
+#define CONFSPLIT_VERSION "1.02"
+#define HELLOVERRIDE_VERSION "1.00"
+#define CMZ_VERSION "1.00"
+#define WHOISTLS_VERSION  "1.00"
+#define CVS3_VERSION "1.00"
+#define PASSWDLEN_VERSION "1.00"
+#define VERSIONLEN_VERSION "1.00"
+#define PASSOPTS_VERSION "1.01"
+#define SPOOF_VERSION "1.05"
+#define WHOISEXTRA_VERSION "1.00"
+#define CCLALL_VERSION "1.00"
+#define VERSION_0PN "1.06"
+#endif
